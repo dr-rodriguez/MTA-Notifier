79f879f2e8503397fbb0213d22fcb28855a48089
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-04-16 12:15:21 -0400

    added weekend notifications

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=20, minute=30)
 def scheduled_job():
     m = notifier.Notifier()
-    m.verbose = False  # Ensure verbosity is set to False
+    m.verbose = True  # Print status of all lines to log
     m.get_status(['123', 'ACE'])
 
     m.send_email_message()
@@ -22,5 +22,14 @@ def scheduled_job():
     m.get_status(['123', 'ACE'])
 
     m.send_email_message()
+
+# Weekend planner
+@sched.scheduled_job('cron', day_of_week='sat', hour=13, minute=30)
+def scheduled_job():
+    m = notifier.Notifier()
+    m.verbose = False
+    m.get_status(['123', 'ACE'], ['GOOD SERVICE', 'DELAYS'])  # Don't notify about delays in addition to good service
+
+    m.send_email_message('MTA Weekend Notifier')  # Explicitly set subject
 
 sched.start()

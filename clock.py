020ddff727ce90a75f7425d06f8d63b4f12e8b69
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-04-25 13:50:26 -0400

    removed schedule for afternoon, added yagmail to requirements

--- clock.py
+++ clock.py
@@ -5,15 +5,6 @@ sched = BlockingScheduler()
 
 # Eastern is 4 hours ahead of UTC, which is what Heroku runs in
 
-# Afternoon check
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=20, minute=30)
-def scheduled_job_afternoon():
-    m = notifier.Notifier()
-    m.verbose = True  # Print status of all lines to log
-    m.get_status(['123', 'ACE'])
-
-    m.send_email_message()
-
 # Morning check
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=12, minute=55)
 def scheduled_job_morning():
